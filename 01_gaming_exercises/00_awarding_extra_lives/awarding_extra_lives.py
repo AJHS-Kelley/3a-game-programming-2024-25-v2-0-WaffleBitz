b6749aa7d0f3a5a6928db77433210bfd76ee0912
WaffleBitz <s12109609@students.duvalschools.org>
2024-09-10 13:20:39 -0400

    Project awarding lives start
    oh boy

---
+++ 01_gaming_exercises/00_awarding_extra_lives/awarding_extra_lives.py
@@ -0,0 +1,26 @@
+# Awarding Extra Lives, Alexander Oropeza-Licona, v0.0
+
+
+lives = 3
+
+# Allow the user to input the score as an interger.
+#The program should output the updated number of lives, and the points scored, as a formatted string.
+
+score = 10
+
+# if score is 10000 or less
+#     Lose a life
+# if score is 10000 but less than 100001
+#     Give 1 Extra Life
+# if score is > 100000
+#     Give 2 Extra Lives
+
+# Output the score and number of lives to the screen.
+
+if score <= 10000:
+    lives - 1 # Lose a life
+elif score is 10000 < 100001:
+    lives + 1 # Give 1 Extra Life
+else score > 100000
+#     Give 2 Extra Lives
+print(str(lives)) # can convert INTEGER TO STRING.
