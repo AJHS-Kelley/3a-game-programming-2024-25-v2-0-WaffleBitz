e07d02cb53f123c4e539c68eb6bbe376363aeb04
WaffleBitz <s12109609@students.duvalschools.org>
2024-09-18 12:04:34 -0400

    Update rock_paper_scissors.py

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -5,9 +5,21 @@ import random
 
 # DATA STRUCTURES -- PLAYER
 player_score = 0
-player_name = input("Test Player")
+player_name = "Test Player"
 player_choice = None
 
 # DATA STRUCTURES -- CPU
 cpu_score = 0
 cpu_choice = None
+
+# PLAYER NAME INPUT
+player_name = input("Type in your name here and press enter.\n")
+print(f"Hello {player_name}!\n")
+is_correct = input("Is that correct? Type yes or no and press enter.\n").lower()
+
+
+
+if is_correct == "yes":
+    pass
+else:
+    player_name = input("Type in your name here and press enter.\n")
