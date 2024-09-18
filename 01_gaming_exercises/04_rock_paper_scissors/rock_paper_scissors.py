c654a1c0bc3d64d1f06d87f9e6c6c79a129f8198
WaffleBitz <s12109609@students.duvalschools.org>
2024-09-18 11:50:31 -0400

    RPS
    Data structures player and cpu

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,13 @@
+# Rock, Paper, Scissors by Alexander Oropeza-Licona, v0.4
+
+# MODULE IMPORTS
+import random
+
+# DATA STRUCTURES -- PLAYER
+player_score = 0
+player_name = input("Test Player")
+player_choice = None
+
+# DATA STRUCTURES -- CPU
+cpu_score = 0
+cpu_choice = None
