cc76f478b33fc50f4bef7542ebc5fc7bde5df193
WaffleBitz <s12109609@students.duvalschools.org>
2024-09-20 11:50:13 -0400

    Update rock_paper_scissors.py
    CPU choices, fixed some problems, and cpu has chosen rock, paper, and scissors at least once

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors.py
@@ -26,7 +26,7 @@ else:
     player_name = input("Type in your name here and press enter.\n")
 
 # THE RULES using MULTI-LINE STRINGS
-print("""
+print(f"""
 Welcome to the Rock, Paper, Scissors Robot!
 It's Time To Play ROCK, PAPER, SCISSORS.
 
@@ -48,17 +48,29 @@ putting a # in front of 15 different lines.
 
 # MAIN GAME LOOP
 while player_score < 5 and cpu_score < 5:
-    print(f"{player_name} you have {player_score} points. \nThe CPU has {cpu_score} points.\n")
+    print(f"{player_name} you have {player_score} points.\nThe CPU has {cpu_score} points.\n")
     player_choice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
-    if player_choice != "rock" or player_choice != "paper" or player_choice != "scissors":
+    if player_choice != "rock" and player_choice != "paper" and player_choice != "scissors":
         player_choice = input("Please enter rock, paper, or scissors and press enter!").lower()
-        if player_choice != "rock" or player_choice != "paper" or player_choice != "scissors":
+        if player_choice != "rock" and player_choice != "paper" and player_choice != "scissors":
             print("You are not follwing directions. Please try again.\n")
-            exit()
-            print(f"you have chosen {player_choice}.\n")
+            exit()  
+        print(f"you have chosen {player_choice}.\n")
     else:
         print(f"you have chosen {player_choice}.\n")
 
+    # let cpu select choice at random.
+    cpu_choice = random.randint(0, 2)# randomly select 0, 1, or 2.
+    if cpu_choice == 0:
+        cpu_choice = "rock"
+    elif cpu_choice == 1:
+        cpu_choice = "paper"
+    elif cpu_choice == 2:
+        cpu_choice = "scissors"
+    else: 
+        print("Unable to determine CPU choice.. \nPlease restart.\n")
+        exit()
+    print(f"CPU Choice: {cpu_choice}")
     # print the current score for CPU and Player
     # let player select rock, paper, scissors,
     # let CPU select choice at random.
