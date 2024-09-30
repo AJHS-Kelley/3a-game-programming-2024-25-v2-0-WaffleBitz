80f6d3b148c50668737b147cf63b5d7fa71586e7
WaffleBitz <s12109609@students.duvalschools.org>
2024-09-30 12:05:00 -0400

    Update rock_paper_scissors_peformance.py

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors_peformance.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors_peformance.py
@@ -13,7 +13,11 @@ cpu_score = 0
 cpu_choice = None
 
 # MAIN GAME LOOP
-while player_score < 5 and cpu_score < 5:
+loop_count = 0
+loops_req = int(input("How many loops do you want?\nEnter an interger, no commas, and press ENTER.\n"))
+# req is the universal abbreviation in computer programming for REQUEST. reqs = REQUESTS
+rps_time_start = time.time() # returns the number of seconds since Jan. 01, 1970 @ 12:00AM
+while loop_count < loops_req:
 
     # let cpu select choice at random.
     cpu_choice = random.randint(0, 2)# randomly select 0, 1, or 2.
@@ -29,13 +33,13 @@ while player_score < 5 and cpu_score < 5:
     # print(f"CPU Choice: {cpu_choice}")
 
     # 1st PLAYER selcet choice at random.
-    cpu_choice = random.randint(0, 2)# randomly select 0, 1, or 2.
-    if cpu_choice == 0:
-        cpu_choice = "rock"
-    elif cpu_choice == 1:
-        cpu_choice = "paper"
-    elif cpu_choice == 2:
-        cpu_choice = "scissors"
+    player_choice = random.randint(0, 2)# randomly select 0, 1, or 2.
+    if player_choice == 0:
+        player_choice = "rock"
+    elif player_choice == 1:
+        player_choice = "paper"
+    elif player_choice == 2:
+        player_choice = "scissors"
     else: 
         print("Unable to determine CPU choice.. \nPlease restart.\n")
         exit()
@@ -67,6 +71,7 @@ while player_score < 5 and cpu_score < 5:
     elif player_choice == "scissors" and cpu_choice == "scissors":
         print(f"The CPU chose {cpu_choice} and you chose {player_choice}.\n")
         print("DRAW.\n")
+        num_draws += 1
     # DRAW
     elif player_choice == "paper" and cpu_choice == "scissors":
         print(f"The CPU chose {cpu_choice} and you chose {player_choice}.\n")
@@ -85,14 +90,19 @@ while player_score < 5 and cpu_score < 5:
     else:
         print("Unable to determine a WINNER. Please Restart.\n")
         exit()
+    loop_count += 1
 
 
-
-
-print(f"Your Final Score: {player_score} CPU Final Score: {cpu_score}\n")
+print(f"Your Final Score: {player_score} CPU Final Score: {cpu_score}\nDraws: {num_draws}\n")
 if player_score > cpu_score:
-    print(f"Congratulations! {player_name}, you won!\n")
+    print(f"Congratulations! , you won!\n")
 elif cpu_score > player_score:
-    print(f"The CPU Wins.. {player_name}, you bring great shame to your family name.\n") 
+    print(f"The CPU Wins.., you bring great shame to your family name.\n") 
 else:
     print("Unable to determine a WINNER. Please Restart.\n")
+    exit()
+
+rps_time_stop = time.time()
+rps_time = rps_time_stop - rps_time_start
+print(f"Number of Loops: {loop_count}\n")
+print(f"Execution Time: {loop_count}\n")
