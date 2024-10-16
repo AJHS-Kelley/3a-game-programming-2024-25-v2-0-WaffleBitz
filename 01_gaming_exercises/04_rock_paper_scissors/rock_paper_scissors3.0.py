5fd774604039839a2bac66fdff760484bfb6d2a0
WaffleBitz <s12109609@students.duvalschools.org>
2024-10-16 13:15:45 -0400

    Update rock_paper_scissors3.0.py

--- 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors3.0.py
+++ 01_gaming_exercises/04_rock_paper_scissors/rock_paper_scissors3.0.py
@@ -13,14 +13,12 @@ cpu_score = 0
 cpu_choice = None
 
 # PLAYER NAME INPUT
-def playerName(): # Function Signature -- name of function, (arguments if any)
+def playerName() -> str: # Function Signature -- name of function, (arguments if any)
+    """Gets the name from the player and returns it."""
+    # The line above is a DOCSTRING, it gives a brief example of what the function does.
     player_name = input("Type in your name here and press enter.\n")
     print(f"Hello {player_name}!\n")
     is_correct = input("Is that correct? Type yes or no and press enter.\n").lower()
-
-    # .lower() can turn ALL input into lowercase.
-    # .upper() can turn ALL input into UPPERCASE.
-
     if is_correct == "yes":
         pass
     else:
@@ -28,32 +26,29 @@ def playerName(): # Function Signature -- name of function, (arguments if any)
     return player_name
 
 # CALLING THE FUNCTION
-playerName()
+player_name = playerName()
 
 # THE RULES using MULTI-LINE STRINGS
-print(f"""
-Welcome to the Rock, Paper, Scissors Robot!
-It's Time To Play ROCK, PAPER, SCISSORS.
+def rules() -> None:
+    """This function prints the rule for rock, paper, scissors."""
+    print(f"""
+    Welcome to the Rock, Paper, Scissors Robot!
+    It's Time To Play ROCK, PAPER, SCISSORS.
 
-You have probably played this thousands of times.. but for those who haven't 
-You will select from ROCK, PAPER, SCISSORS. Your Opponent, The CPU, will select from ROCK, PAPER, SCISSORS at random.
-First to score 5 points wins
-      
-1) ROCK BEATS SCISSORS
-2) SCISSORS BEATS PAPER
-3) PAPER BEATS ROCK
-""")
+    You have probably played this thousands of times.. but for those who haven't 
+    You will select from ROCK, PAPER, SCISSORS. Your Opponent, The CPU, will select from ROCK, PAPER, SCISSORS at random.
+    First to score 5 points wins
 
-# MULTI-LINE STRINGS CAN BE USED AS BIG COMMENTS
-"""
-Anything in between the sets of double-quotes is just ignored.
-If you need to write large comments, it's easier to use multi-line strings than
-putting a # in front of 15 different lines.
-"""
+    1) ROCK BEATS SCISSORS
+    2) SCISSORS BEATS PAPER
+    3) PAPER BEATS ROCK
+    """)
+    # Does another part of this program need to access this imformation?
+    # IF YES, YOU MUST HAVE A return STATEMENT
+    # IF NO, A return STATEMENT IS NOT REQUIRED
 
-# MAIN GAME LOOP
-while player_score < 5 and cpu_score < 5:
-    print(f"{player_name} you have {player_score} points.\nThe CPU has {cpu_score} points.\n")
+def playerChoice() -> str:
+    """Allows the player to chose rock, paper, scissors."""
     player_choice = input("Please enter rock, paper, or scissors and press enter.\n").lower()
     if player_choice != "rock" and player_choice != "paper" and player_choice != "scissors":
         player_choice = input("Please enter rock, paper, or scissors and press enter!").lower()
@@ -63,8 +58,10 @@ while player_score < 5 and cpu_score < 5:
         print(f"you have chosen {player_choice}.\n")
     else:
         print(f"you have chosen {player_choice}.\n")
-
-    # let cpu select choice at random.
+        return player_choice
+    
+def cpuChoice() -> str:
+    """Allows the CPU to chose rock, paper, scissors."""
     cpu_choice = random.randint(0, 2)# randomly select 0, 1, or 2.
     if cpu_choice == 0:
         cpu_choice = "rock"
@@ -75,7 +72,15 @@ while player_score < 5 and cpu_score < 5:
     else: 
         print("Unable to determine CPU choice.. \nPlease restart.\n")
         exit()
-    # print(f"CPU Choice: {cpu_choice}")
+    return cpu_choice
+
+# MAIN GAME LOOP
+while player_score < 5 and cpu_score < 5:
+    print(f"{player_name} you have {player_score} points.\nThe CPU has {cpu_score} points.\n")
+    
+  
+    # let cpu select choice at random.
+   
 
 # compare player choice to cpu choice
     if player_choice == "rock" and cpu_choice == "paper":
